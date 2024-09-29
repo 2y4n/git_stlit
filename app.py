207002c1a97a143a9d95769808d35d183d568083
2y4n <163217935+2y4n@users.noreply.github.com>
2024-09-29 20:14:10 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import joblib
+
+# st.title("Rayan")
+# model = joblib.load("svc.pkl")
+
+# st.write('##Loan Prediction')
+
+# x = st.slider('experience', 0 , 40)
+# # Predict accepts only 2 dimension
+# # Do reshape or like the bottom line
+# y = model.predict([[x]])
+
+# st.write('Loan Condition is')
+
+st.title("Rayan")
+model = joblib.load("svc.pkl")
+
+st.write('BMI')
+
+hieght = ((st.slider('Height', 1 , 200))*10)**2
+wieght  = st.slider('Weight :', 1, 200)
+bmi = wieght / (hieght * hieght)
+st.write('Your BMI is :', (bmi))
